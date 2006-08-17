92178ea959ef08f2e1b14e08cb7b5e70c82d4607
Greg Beaver <cellog@php.net>
2006-08-17 03:23:26 +0000

    fix sourceforge bug  [ 1516781 ] Use of undefined constant T_ML_COMMENT - assumed 'T_ML_COMME
    git-svn-id: http://svn.php.net/repository/pear/packages/PhpDocumentor/trunk@218326 c90b9560-bf6c-de11-be94-00142212c4b1

--- scripts/create_package.xml.php
+++ scripts/create_package.xml.php
@@ -80,8 +80,9 @@ segfaults with the simplest of files.  Generation still works great in PHP4
  [ 1497452 ] Fatal error: Call to undefined method parserFunction::setMo
  [ 1533361 ] trailing comments stop phpdoc on source generation
  [ 1532841 ] \'%\' in function parameter default value causes errors
- [ 1528752 ] Trivial typo in "phpDocumentor Inline tags" documentation
+ [ 1516781 ] Use of undefined constant T_ML_COMMENT - assumed \'T_ML_COMME
  [ 1524102 ] Weird include documentation
+ [ 1528752 ] Trivial typo in "phpDocumentor Inline tags" documentation
  Bug #6844: phpDocumentor doesn\'t show classes in file
  Bug #7554: Call to undefined method ReflectionClass::hasMethod()
  Bug #7577: Notice error for undefined _pv_function_param property
@@ -191,8 +192,9 @@ segfaults with the simplest of files.  Generation still works great in PHP4
  [ 1497452 ] Fatal error: Call to undefined method parserFunction::setMo
  [ 1533361 ] trailing comments stop phpdoc on source generation
  [ 1532841 ] \'%\' in function parameter default value causes errors
- [ 1528752 ] Trivial typo in "phpDocumentor Inline tags" documentation
+ [ 1516781 ] Use of undefined constant T_ML_COMMENT - assumed \'T_ML_COMME
  [ 1524102 ] Weird include documentation
+ [ 1528752 ] Trivial typo in "phpDocumentor Inline tags" documentation
 - fixed these bugs reported in PEAR:
  Bug #2122: No tree menu for file identifier begining with a number
  Bug #2288: Webfrontend ignores more than one dir in "Files to ignore"
