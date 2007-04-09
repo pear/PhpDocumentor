48ca57986a9cead4722738e8e9a71c3c880f2e32
Chuck Burgess <ashnazg@php.net>
2007-04-09 17:48:09 +0000

    update converter name cleaner code to deal with DOM/blahblah converters
    git-svn-id: http://svn.php.net/repository/pear/packages/PhpDocumentor/trunk@233506 c90b9560-bf6c-de11-be94-00142212c4b1

--- Documentation/tests/bug1620470.php
+++ Documentation/tests/bug1620470.php
@@ -57,44 +57,67 @@ class Documentation_tests_bug1620470 extends PHPUnit_Framework_TestCase {
      */
     public function testCleanConverterNamePiece() {
 
+        define('CHARACTERS_ALLOWED_IN_PRIMARY', '');
+        define('CHARACTERS_ALLOWED_IN_SECONDARY', '\/');
+        define('CHARACTERS_ALLOWED_IN_TERTIARY', '.\/');
+
         // verify all existing converter names are accepted
-        //     CHM, HTML, PDF, XML
+        //     CHM, HTML, PDF, XML (called with one arg)
         $this->assertEquals("CHM",              $this->ps->cleanConverterNamePiece("CHM"));
         $this->assertEquals("HTML",             $this->ps->cleanConverterNamePiece("HTML"));
         $this->assertEquals("PDF",              $this->ps->cleanConverterNamePiece("PDF"));
         $this->assertEquals("XML",              $this->ps->cleanConverterNamePiece("XML"));
+        //     CHM, HTML, PDF, XML (called with two args)
+        $this->assertEquals("CHM",              $this->ps->cleanConverterNamePiece("CHM",       CHARACTERS_ALLOWED_IN_PRIMARY));
+        $this->assertEquals("HTML",             $this->ps->cleanConverterNamePiece("HTML",      CHARACTERS_ALLOWED_IN_PRIMARY));
+        $this->assertEquals("PDF",              $this->ps->cleanConverterNamePiece("PDF",       CHARACTERS_ALLOWED_IN_PRIMARY));
+        $this->assertEquals("XML",              $this->ps->cleanConverterNamePiece("XML",       CHARACTERS_ALLOWED_IN_PRIMARY));
+
         //     frames, Smarty, default, DocBook/peardoc2
-        $this->assertEquals("frames",           $this->ps->cleanConverterNamePiece("frames", "\/"));
-        $this->assertEquals("Smarty",           $this->ps->cleanConverterNamePiece("Smarty", "\/"));
-        $this->assertEquals("default",          $this->ps->cleanConverterNamePiece("default", "\/"));
-        $this->assertEquals("DocBook/peardoc2", $this->ps->cleanConverterNamePiece("DocBook/peardoc2", "\/"));        
+        $this->assertEquals("frames",           $this->ps->cleanConverterNamePiece("frames",    CHARACTERS_ALLOWED_IN_SECONDARY));
+        $this->assertEquals("Smarty",           $this->ps->cleanConverterNamePiece("Smarty",    CHARACTERS_ALLOWED_IN_SECONDARY));
+        $this->assertEquals("default",          $this->ps->cleanConverterNamePiece("default",   CHARACTERS_ALLOWED_IN_SECONDARY));
+        $this->assertEquals("DocBook/peardoc2", $this->ps->cleanConverterNamePiece("DocBook/peardoc2", CHARACTERS_ALLOWED_IN_SECONDARY));
+
         //     default, earthli, l0l33t, phpdoc.de, phphtmllib, HandS, PEAR, PHP
-        $this->assertEquals("default",          $this->ps->cleanConverterNamePiece("default", "."));
-        $this->assertEquals("earthli",          $this->ps->cleanConverterNamePiece("earthli", "."));
-        $this->assertEquals("l0l33t",           $this->ps->cleanConverterNamePiece("l0l33t", "."));
-        $this->assertEquals("phpdoc.de",        $this->ps->cleanConverterNamePiece("phpdoc.de", "."));
-        $this->assertEquals("phphtmllib",       $this->ps->cleanConverterNamePiece("phphtmllib", "."));
-        $this->assertEquals("HandS",            $this->ps->cleanConverterNamePiece("HandS", "."));
-        $this->assertEquals("PEAR",             $this->ps->cleanConverterNamePiece("PEAR", "."));
-        $this->assertEquals("PHP",              $this->ps->cleanConverterNamePiece("PHP", "."));
+        $this->assertEquals("default",          $this->ps->cleanConverterNamePiece("default",   CHARACTERS_ALLOWED_IN_TERTIARY));
+        $this->assertEquals("earthli",          $this->ps->cleanConverterNamePiece("earthli",   CHARACTERS_ALLOWED_IN_TERTIARY));
+        $this->assertEquals("l0l33t",           $this->ps->cleanConverterNamePiece("l0l33t",    CHARACTERS_ALLOWED_IN_TERTIARY));
+        $this->assertEquals("phpdoc.de",        $this->ps->cleanConverterNamePiece("phpdoc.de", CHARACTERS_ALLOWED_IN_TERTIARY));
+        $this->assertEquals("phphtmllib",       $this->ps->cleanConverterNamePiece("phphtmllib",CHARACTERS_ALLOWED_IN_TERTIARY));
+        $this->assertEquals("HandS",            $this->ps->cleanConverterNamePiece("HandS",     CHARACTERS_ALLOWED_IN_TERTIARY));
+        $this->assertEquals("PEAR",             $this->ps->cleanConverterNamePiece("PEAR",      CHARACTERS_ALLOWED_IN_TERTIARY));
+        $this->assertEquals("PHP",              $this->ps->cleanConverterNamePiece("PHP",       CHARACTERS_ALLOWED_IN_TERTIARY));
+
+        //     DOM option for default, earthli, l0l33t, phpdoc.de, phphtmllib
+        $this->assertEquals("DOM/default",      $this->ps->cleanConverterNamePiece("DOM/default",    CHARACTERS_ALLOWED_IN_TERTIARY));
+        $this->assertEquals("DOM/earthli",      $this->ps->cleanConverterNamePiece("DOM/earthli",    CHARACTERS_ALLOWED_IN_TERTIARY));
+        $this->assertEquals("DOM/l0l33t",       $this->ps->cleanConverterNamePiece("DOM/l0l33t",     CHARACTERS_ALLOWED_IN_TERTIARY));
+        $this->assertEquals("DOM/phpdoc.de",    $this->ps->cleanConverterNamePiece("DOM/phpdoc.de",  CHARACTERS_ALLOWED_IN_TERTIARY));
+        $this->assertEquals("DOM/phphtmllib",   $this->ps->cleanConverterNamePiece("DOM/phphtmllib", CHARACTERS_ALLOWED_IN_TERTIARY));
 
         // verify no up-to-parent pathing is allowed - the resulting converter names are generally invalid
-        $this->assertEquals("HTML",            $this->ps->cleanConverterNamePiece("../../HTML"));
-        $this->assertEquals("//frames",        $this->ps->cleanConverterNamePiece("../../frames", "\/"));
-        $this->assertEquals("....default",     $this->ps->cleanConverterNamePiece("../../default", "."));
-                        
-        $this->assertEquals("varlogHTML",      $this->ps->cleanConverterNamePiece("/var/log/HTML"));
-        $this->assertEquals("/var/log/frames", $this->ps->cleanConverterNamePiece("/var/log/frames", "\/"));
-        $this->assertEquals("varlogdefault",   $this->ps->cleanConverterNamePiece("/var/log/default", "."));
+        $this->assertEquals("HTML",             $this->ps->cleanConverterNamePiece("../../HTML"));
+        $this->assertEquals("HTML",             $this->ps->cleanConverterNamePiece("../../HTML",   CHARACTERS_ALLOWED_IN_PRIMARY));
+        $this->assertEquals("//frames",         $this->ps->cleanConverterNamePiece("../../frames", CHARACTERS_ALLOWED_IN_SECONDARY));
+        //    when '.' is allowed to remain, a '..' always returns false to avoid directory traversal
+        $this->assertEquals(false,              $this->ps->cleanConverterNamePiece("../../default",CHARACTERS_ALLOWED_IN_TERTIARY));
+
+        $this->assertEquals("varlogHTML",       $this->ps->cleanConverterNamePiece("/var/log/HTML"));
+        $this->assertEquals("varlogHTML",       $this->ps->cleanConverterNamePiece("/var/log/HTML",   CHARACTERS_ALLOWED_IN_PRIMARY));
+        $this->assertEquals("/var/log/frames",  $this->ps->cleanConverterNamePiece("/var/log/frames", CHARACTERS_ALLOWED_IN_SECONDARY));
+        $this->assertEquals("/var/log/default", $this->ps->cleanConverterNamePiece("/var/log/default",CHARACTERS_ALLOWED_IN_TERTIARY));
 
         // extreme examples of messy input - the resulting converter names are valid
-        $this->assertEquals("HTML",            $this->ps->cleanConverterNamePiece("H/.T./M##L"));
-        $this->assertEquals("frames",          $this->ps->cleanConverterNamePiece("....frames", "\/"));
-        $this->assertEquals("default",         $this->ps->cleanConverterNamePiece("//default//", "."));
-        
+        $this->assertEquals("HTML",             $this->ps->cleanConverterNamePiece("H/.T./M##L"));
+        $this->assertEquals("HTML",             $this->ps->cleanConverterNamePiece("H/.T./M##L", CHARACTERS_ALLOWED_IN_PRIMARY));
+        $this->assertEquals("frames",           $this->ps->cleanConverterNamePiece("....frames", CHARACTERS_ALLOWED_IN_SECONDARY));
+
         // extreme examples of messy input - the resulting converter names are invalid
-        $this->assertEquals("//frames",        $this->ps->cleanConverterNamePiece("..//.frames",  "\/"));
-        $this->assertEquals(".default.",       $this->ps->cleanConverterNamePiece("/./default/.##/",  "."));   
+        $this->assertEquals("//frames",         $this->ps->cleanConverterNamePiece("..//.frames",     CHARACTERS_ALLOWED_IN_SECONDARY));
+        $this->assertEquals("/./default/./",    $this->ps->cleanConverterNamePiece("/./default/.##/", CHARACTERS_ALLOWED_IN_TERTIARY));
+        $this->assertEquals("//default//",      $this->ps->cleanConverterNamePiece("//default//",     CHARACTERS_ALLOWED_IN_TERTIARY));
+
     }
 
 }
