fe5265dc6486dd168dce49fda93a3c2a74dcc967
Chuck Burgess <ashnazg@php.net>
2007-04-20 19:49:32 +0000

    split assertions out into individual test functions, for more precise unit testing; correct main() classname;
    git-svn-id: http://svn.php.net/repository/pear/packages/PhpDocumentor/trunk@234192 c90b9560-bf6c-de11-be94-00142212c4b1

--- Documentation/tests/bug1602961.php
+++ Documentation/tests/bug1602961.php
@@ -10,7 +10,8 @@ require_once "PHPUnit/Framework/TestSuite.php";
 require_once 'phpDocumentor/Setup.inc.php';
 
 /**
- * Test class for phpDocumentor_setup.
+ * verify the fuzzy logic that guesses
+ * the intent of boolean arg values 
  */
 class Documentation_tests_bug1602961 extends PHPUnit_Framework_TestCase {
 
@@ -25,7 +26,7 @@ class Documentation_tests_bug1602961 extends PHPUnit_Framework_TestCase {
     public static function main() {
         require_once "PHPUnit/TextUI/TestRunner.php";
 
-        $suite  = new PHPUnit_Framework_TestSuite("phpDocumentor_setupTest");
+        $suite  = new PHPUnit_Framework_TestSuite("Documentation_tests_bug1602961");
         $result = PHPUnit_TextUI_TestRunner::run($suite);
     }
 
@@ -47,56 +48,109 @@ class Documentation_tests_bug1602961 extends PHPUnit_Framework_TestCase {
      * @access protected
      */
     protected function tearDown() {
+        unset($ps);
     }
 
-    /**
-     * verify the fuzzy logic that guesses
-     * the intent of boolean arg values 
-     */
-    public function testDecideOnOrOff() {
 
-        // verify all expected values are interpreted correctly
+    // verify all expected values are interpreted correctly
+    public function testBasicOff() {
         $this->assertFalse(decideOnOrOff('off'));
+    }
+    public function testBasicOn() {
         $this->assertTrue(decideOnOrOff('on'));
+    }
         
-        // verify all the fuzzy values we know to recognize
-        //   no
+    /**
+     * verify all the fuzzy values we know to recognize
+     */
+    // no
+    public function testFuzzyOffA() {
         $this->assertFalse(decideOnOrOff('Off'));
+    }
+    public function testFuzzyOffB() {
         $this->assertFalse(decideOnOrOff('OFF'));
+    }
+    public function testFuzzyNoA() {
         $this->assertFalse(decideOnOrOff('no'));
+    }
+    public function testFuzzyNoB() {
         $this->assertFalse(decideOnOrOff('No'));
+    }
+    public function testFuzzyNoC() {
         $this->assertFalse(decideOnOrOff('NO'));
+    }
+    public function testFuzzyFalseA() {
         $this->assertFalse(decideOnOrOff('false'));
+    }
+    public function testFuzzyFalseB() {
         $this->assertFalse(decideOnOrOff('False'));
+    }
+    public function testFuzzyFalseC() {
         $this->assertFalse(decideOnOrOff('FALSE'));
+    }
+    public function testFuzzyZero() {
         $this->assertFalse(decideOnOrOff(0));
-                
-        //   yes
+    }
+
+    //   yes
+    public function testFuzzyEmpty() {
         $this->assertTrue(decideOnOrOff(''));
+    }
+    public function testFuzzyOnA() {
         $this->assertTrue(decideOnOrOff('On'));
+    }
+    public function testFuzzyOnB() {
         $this->assertTrue(decideOnOrOff('ON'));
+    }
+    public function testFuzzyYesA() {
         $this->assertTrue(decideOnOrOff('y'));
+    }
+    public function testFuzzyYesB() {
         $this->assertTrue(decideOnOrOff('Y'));
+    }
+    public function testFuzzyYesC() {
         $this->assertTrue(decideOnOrOff('yes'));
+    }
+    public function testFuzzyYesD() {
         $this->assertTrue(decideOnOrOff('Yes'));
+    }
+    public function testFuzzyYesE() {
         $this->assertTrue(decideOnOrOff('YES'));
+    }
+    public function testFuzzyTrueA() {
         $this->assertTrue(decideOnOrOff('true'));
+    }
+    public function testFuzzyTrueB() {
         $this->assertTrue(decideOnOrOff('True'));
+    }
+    public function testFuzzyTrueC() {
         $this->assertTrue(decideOnOrOff('TRUE'));
+    }
+    public function testFuzzyOne() {
         $this->assertTrue(decideOnOrOff(1));
+    }
                 
-        // unexpected
+    // unexpected
+    public function testUnexpectedSpaces() {
         $this->assertTrue(decideOnOrOff('  '));
+    }
+    public function testUnexpectedNull() {
         $this->assertFalse(decideOnOrOff());
+    }
+    public function testUnexpectedNegative() {
         $this->assertFalse(decideOnOrOff(-1));
+    }
+    public function testUnexpectedLargeNumber() {
         $this->assertFalse(decideOnOrOff(10));
+    }
+    public function testUnexpectedGreatLiterature() {
         $this->assertFalse(decideOnOrOff("ash nazg durbatuluk"));
     }
 
 }
 
 // Call phpDocumentor_setupTest::main() if this source file is executed directly.
-if (PHPUnit_MAIN_METHOD == "phpDocumentor_setupTest::main") {
+if (PHPUnit_MAIN_METHOD == "Documentation_tests_bug1602961::main") {
     phpDocumentor_setupTest::main();
 }
 ?>
--- Documentation/tests/bug1620470.php
+++ Documentation/tests/bug1620470.php
@@ -10,11 +10,15 @@ require_once "PHPUnit/Framework/TestSuite.php";
 require_once 'phpDocumentor/Setup.inc.php';
 
 /**
- * Test class for phpDocumentor_setup.
+ * verify the anti-pathing security measures
+ * on the various converter name pieces 
  */
 class Documentation_tests_bug1620470 extends PHPUnit_Framework_TestCase {
 
     private $ps;
+    private $CHARACTERS_ALLOWED_IN_PRIMARY = '';
+    private $CHARACTERS_ALLOWED_IN_SECONDARY = '\/';
+    private $CHARACTERS_ALLOWED_IN_TERTIARY = '.\/';
 
     /**
      * Runs the test methods of this class.
@@ -25,7 +29,7 @@ class Documentation_tests_bug1620470 extends PHPUnit_Framework_TestCase {
     public static function main() {
         require_once "PHPUnit/TextUI/TestRunner.php";
 
-        $suite  = new PHPUnit_Framework_TestSuite("phpDocumentor_setupTest");
+        $suite  = new PHPUnit_Framework_TestSuite("Documentation_tests_bug1620470");
         $result = PHPUnit_TextUI_TestRunner::run($suite);
     }
 
@@ -49,81 +53,168 @@ class Documentation_tests_bug1620470 extends PHPUnit_Framework_TestCase {
      * @access protected
      */
     protected function tearDown() {
+        unset($CHARACTERS_ALLOWED_IN_PRIMARY);
+        unset($CHARACTERS_ALLOWED_IN_SECONDARY);
+        unset($CHARACTERS_ALLOWED_IN_TERTIARY);
+        unset($ps);
     }
 
-    /**
-     * verify the anti-pathing security measures
-     * on the various converter name pieces 
+
+
+    /** 
+     * verify all existing converter names are accepted 
      */
-    public function testCleanConverterNamePiece() {
 
-        define('CHARACTERS_ALLOWED_IN_PRIMARY', '');
-        define('CHARACTERS_ALLOWED_IN_SECONDARY', '\/');
-        define('CHARACTERS_ALLOWED_IN_TERTIARY', '.\/');
-
-        // verify all existing converter names are accepted
-        //     CHM, HTML, PDF, XML (called with one arg)
+    //     CHM, HTML, PDF, XML (called with one arg)
+    public function testNormalWithOneArgPrimaryCHM() {
         $this->assertEquals("CHM",              $this->ps->cleanConverterNamePiece("CHM"));
+    }
+    public function testNormalWithOneArgPrimaryHTML() {
         $this->assertEquals("HTML",             $this->ps->cleanConverterNamePiece("HTML"));
+    }
+    public function testNormalWithOneArgPrimaryPDF() {
         $this->assertEquals("PDF",              $this->ps->cleanConverterNamePiece("PDF"));
+    }
+    public function testNormalWithOneArgPrimaryXML() {
         $this->assertEquals("XML",              $this->ps->cleanConverterNamePiece("XML"));
-        //     CHM, HTML, PDF, XML (called with two args)
-        $this->assertEquals("CHM",              $this->ps->cleanConverterNamePiece("CHM",       CHARACTERS_ALLOWED_IN_PRIMARY));
-        $this->assertEquals("HTML",             $this->ps->cleanConverterNamePiece("HTML",      CHARACTERS_ALLOWED_IN_PRIMARY));
-        $this->assertEquals("PDF",              $this->ps->cleanConverterNamePiece("PDF",       CHARACTERS_ALLOWED_IN_PRIMARY));
-        $this->assertEquals("XML",              $this->ps->cleanConverterNamePiece("XML",       CHARACTERS_ALLOWED_IN_PRIMARY));
+    }
 
-        //     frames, Smarty, default, DocBook/peardoc2
-        $this->assertEquals("frames",           $this->ps->cleanConverterNamePiece("frames",    CHARACTERS_ALLOWED_IN_SECONDARY));
-        $this->assertEquals("Smarty",           $this->ps->cleanConverterNamePiece("Smarty",    CHARACTERS_ALLOWED_IN_SECONDARY));
-        $this->assertEquals("default",          $this->ps->cleanConverterNamePiece("default",   CHARACTERS_ALLOWED_IN_SECONDARY));
-        $this->assertEquals("DocBook/peardoc2", $this->ps->cleanConverterNamePiece("DocBook/peardoc2", CHARACTERS_ALLOWED_IN_SECONDARY));
+    //     CHM, HTML, PDF, XML (called with two args)
+    public function testNormalWithTwoArgsPrimaryCHM() {
+        $this->assertEquals("CHM",              $this->ps->cleanConverterNamePiece("CHM",       $this->CHARACTERS_ALLOWED_IN_PRIMARY));
+    }
+    public function testNormalWithTwoArgsPrimaryHTML() {
+        $this->assertEquals("HTML",             $this->ps->cleanConverterNamePiece("HTML",      $this->CHARACTERS_ALLOWED_IN_PRIMARY));
+    }
+    public function testNormalWithTwoArgsPrimaryPDF() {
+        $this->assertEquals("PDF",              $this->ps->cleanConverterNamePiece("PDF",       $this->CHARACTERS_ALLOWED_IN_PRIMARY));
+    }
+    public function testNormalWithTwoArgsPrimaryXML() {
+        $this->assertEquals("XML",              $this->ps->cleanConverterNamePiece("XML",       $this->CHARACTERS_ALLOWED_IN_PRIMARY));
+    }
 
-        //     default, earthli, l0l33t, phpdoc.de, phphtmllib, HandS, PEAR, PHP
-        $this->assertEquals("default",          $this->ps->cleanConverterNamePiece("default",   CHARACTERS_ALLOWED_IN_TERTIARY));
-        $this->assertEquals("earthli",          $this->ps->cleanConverterNamePiece("earthli",   CHARACTERS_ALLOWED_IN_TERTIARY));
-        $this->assertEquals("l0l33t",           $this->ps->cleanConverterNamePiece("l0l33t",    CHARACTERS_ALLOWED_IN_TERTIARY));
-        $this->assertEquals("phpdoc.de",        $this->ps->cleanConverterNamePiece("phpdoc.de", CHARACTERS_ALLOWED_IN_TERTIARY));
-        $this->assertEquals("phphtmllib",       $this->ps->cleanConverterNamePiece("phphtmllib",CHARACTERS_ALLOWED_IN_TERTIARY));
-        $this->assertEquals("HandS",            $this->ps->cleanConverterNamePiece("HandS",     CHARACTERS_ALLOWED_IN_TERTIARY));
-        $this->assertEquals("PEAR",             $this->ps->cleanConverterNamePiece("PEAR",      CHARACTERS_ALLOWED_IN_TERTIARY));
-        $this->assertEquals("PHP",              $this->ps->cleanConverterNamePiece("PHP",       CHARACTERS_ALLOWED_IN_TERTIARY));
+    //     frames, Smarty, default, DocBook/peardoc2
+    public function testNormalSecondaryFrames() {
+        $this->assertEquals("frames",           $this->ps->cleanConverterNamePiece("frames",    $this->CHARACTERS_ALLOWED_IN_SECONDARY));
+    }
+    public function testNormalSecondarySmarty() {
+        $this->assertEquals("Smarty",           $this->ps->cleanConverterNamePiece("Smarty",    $this->CHARACTERS_ALLOWED_IN_SECONDARY));
+    }
+    public function testNormalSecondaryDefault() {
+        $this->assertEquals("default",          $this->ps->cleanConverterNamePiece("default",   $this->CHARACTERS_ALLOWED_IN_SECONDARY));
+    }
+    public function testNormalSecondaryDocbookPeardoc2() {
+        $this->assertEquals("DocBook/peardoc2", $this->ps->cleanConverterNamePiece("DocBook/peardoc2", $this->CHARACTERS_ALLOWED_IN_SECONDARY));
+    }
 
-        //     DOM option for default, earthli, l0l33t, phpdoc.de, phphtmllib
-        $this->assertEquals("DOM/default",      $this->ps->cleanConverterNamePiece("DOM/default",    CHARACTERS_ALLOWED_IN_TERTIARY));
-        $this->assertEquals("DOM/earthli",      $this->ps->cleanConverterNamePiece("DOM/earthli",    CHARACTERS_ALLOWED_IN_TERTIARY));
-        $this->assertEquals("DOM/l0l33t",       $this->ps->cleanConverterNamePiece("DOM/l0l33t",     CHARACTERS_ALLOWED_IN_TERTIARY));
-        $this->assertEquals("DOM/phpdoc.de",    $this->ps->cleanConverterNamePiece("DOM/phpdoc.de",  CHARACTERS_ALLOWED_IN_TERTIARY));
-        $this->assertEquals("DOM/phphtmllib",   $this->ps->cleanConverterNamePiece("DOM/phphtmllib", CHARACTERS_ALLOWED_IN_TERTIARY));
+    //     default, earthli, l0l33t, phpdoc.de, phphtmllib, HandS, PEAR, PHP
+    public function testNormalTertiaryDefault() {
+        $this->assertEquals("default",          $this->ps->cleanConverterNamePiece("default",   $this->CHARACTERS_ALLOWED_IN_TERTIARY));
+    }
+    public function testNormalTertiaryEarthli() {
+        $this->assertEquals("earthli",          $this->ps->cleanConverterNamePiece("earthli",   $this->CHARACTERS_ALLOWED_IN_TERTIARY));
+    }
+    public function testNormalTertiaryL0l33t() {
+        $this->assertEquals("l0l33t",           $this->ps->cleanConverterNamePiece("l0l33t",    $this->CHARACTERS_ALLOWED_IN_TERTIARY));
+    }
+    public function testNormalTertiaryPhpdocde() {
+        $this->assertEquals("phpdoc.de",        $this->ps->cleanConverterNamePiece("phpdoc.de", $this->CHARACTERS_ALLOWED_IN_TERTIARY));
+    }
+    public function testNormalTertiaryPhphtmllib() {
+        $this->assertEquals("phphtmllib",       $this->ps->cleanConverterNamePiece("phphtmllib",$this->CHARACTERS_ALLOWED_IN_TERTIARY));
+    }
+    public function testNormalTertiaryHands() {
+        $this->assertEquals("HandS",            $this->ps->cleanConverterNamePiece("HandS",     $this->CHARACTERS_ALLOWED_IN_TERTIARY));
+    }
+    public function testNormalTertiaryPear() {
+        $this->assertEquals("PEAR",             $this->ps->cleanConverterNamePiece("PEAR",      $this->CHARACTERS_ALLOWED_IN_TERTIARY));
+    }
+    public function testNormalTertiaryPhp() {
+        $this->assertEquals("PHP",              $this->ps->cleanConverterNamePiece("PHP",       $this->CHARACTERS_ALLOWED_IN_TERTIARY));
+    }
 
-        // verify no up-to-parent pathing is allowed - the resulting converter names are generally invalid
+    //     DOM option for default, earthli, l0l33t, phpdoc.de, phphtmllib
+    public function testNormalTertiaryDomDefault() {
+        $this->assertEquals("DOM/default",      $this->ps->cleanConverterNamePiece("DOM/default",    $this->CHARACTERS_ALLOWED_IN_TERTIARY));
+    }
+    public function testNormalTertiaryDomEarthli() {
+        $this->assertEquals("DOM/earthli",      $this->ps->cleanConverterNamePiece("DOM/earthli",    $this->CHARACTERS_ALLOWED_IN_TERTIARY));
+    }
+    public function testNormalTertiaryDomL0l33t() {
+        $this->assertEquals("DOM/l0l33t",       $this->ps->cleanConverterNamePiece("DOM/l0l33t",     $this->CHARACTERS_ALLOWED_IN_TERTIARY));
+    }
+    public function testNormalTertiaryDomPhpdocde() {
+        $this->assertEquals("DOM/phpdoc.de",    $this->ps->cleanConverterNamePiece("DOM/phpdoc.de",  $this->CHARACTERS_ALLOWED_IN_TERTIARY));
+    }
+    public function testNormalTertiaryDomPhphtmllib() {
+        $this->assertEquals("DOM/phphtmllib",   $this->ps->cleanConverterNamePiece("DOM/phphtmllib", $this->CHARACTERS_ALLOWED_IN_TERTIARY));
+    }
+
+
+
+    /** 
+     * verify no up-to-parent pathing is allowed - the resulting converter names are generally invalid
+     */
+    public function testPreventUpToParentPathingOnPrimaryWithOneArg() {
         $this->assertEquals("HTML",             $this->ps->cleanConverterNamePiece("../../HTML"));
-        $this->assertEquals("HTML",             $this->ps->cleanConverterNamePiece("../../HTML",   CHARACTERS_ALLOWED_IN_PRIMARY));
-        $this->assertEquals("//frames",         $this->ps->cleanConverterNamePiece("../../frames", CHARACTERS_ALLOWED_IN_SECONDARY));
+    }
+    public function testPreventUpToParentPathingOnPrimaryWithTwoArgs() {
+        $this->assertEquals("HTML",             $this->ps->cleanConverterNamePiece("../../HTML",   $this->CHARACTERS_ALLOWED_IN_PRIMARY));
+    }
+    public function testPreventUpToParentPathingOnSecondary() {
+        $this->assertEquals("//frames",         $this->ps->cleanConverterNamePiece("../../frames", $this->CHARACTERS_ALLOWED_IN_SECONDARY));
+    }
+    public function testPreventUpToParentPathingOnTertiary() {
         //    when '.' is allowed to remain, a '..' always returns false to avoid directory traversal
-        $this->assertEquals(false,              $this->ps->cleanConverterNamePiece("../../default",CHARACTERS_ALLOWED_IN_TERTIARY));
-
+        $this->assertEquals(false,              $this->ps->cleanConverterNamePiece("../../default",$this->CHARACTERS_ALLOWED_IN_TERTIARY));
+    }
+    public function testDoNotAllowTruePathingOnPrimaryWithOneArg() {
         $this->assertEquals("varlogHTML",       $this->ps->cleanConverterNamePiece("/var/log/HTML"));
-        $this->assertEquals("varlogHTML",       $this->ps->cleanConverterNamePiece("/var/log/HTML",   CHARACTERS_ALLOWED_IN_PRIMARY));
-        $this->assertEquals("/var/log/frames",  $this->ps->cleanConverterNamePiece("/var/log/frames", CHARACTERS_ALLOWED_IN_SECONDARY));
-        $this->assertEquals("/var/log/default", $this->ps->cleanConverterNamePiece("/var/log/default",CHARACTERS_ALLOWED_IN_TERTIARY));
+    }
+    public function testDoNotAllowTruePathingOnPrimaryWithTwoArgs() {
+        $this->assertEquals("varlogHTML",       $this->ps->cleanConverterNamePiece("/var/log/HTML",   $this->CHARACTERS_ALLOWED_IN_PRIMARY));
+    }
+    public function testDoNotAllowTruePathingOnSecondary() {
+        $this->assertEquals("/var/log/frames",  $this->ps->cleanConverterNamePiece("/var/log/frames", $this->CHARACTERS_ALLOWED_IN_SECONDARY));
+    }
+    public function testDoNotAllowTruePathingOnTertiary() {
+        $this->assertEquals("/var/log/default", $this->ps->cleanConverterNamePiece("/var/log/default",$this->CHARACTERS_ALLOWED_IN_TERTIARY));
+    }
 
-        // extreme examples of messy input - the resulting converter names are valid
+
+
+    /**
+     * extreme examples of messy input - the resulting converter names are valid
+     */
+    public function testExtremeExampleButValidPrimaryWithOneArg() {
         $this->assertEquals("HTML",             $this->ps->cleanConverterNamePiece("H/.T./M##L"));
-        $this->assertEquals("HTML",             $this->ps->cleanConverterNamePiece("H/.T./M##L", CHARACTERS_ALLOWED_IN_PRIMARY));
-        $this->assertEquals("frames",           $this->ps->cleanConverterNamePiece("....frames", CHARACTERS_ALLOWED_IN_SECONDARY));
+    }
+    public function testExtremeExampleButValidPrimaryWithTwoArgs() {
+        $this->assertEquals("HTML",             $this->ps->cleanConverterNamePiece("H/.T./M##L", $this->CHARACTERS_ALLOWED_IN_PRIMARY));
+    }
+    public function testExtremeExampleButValidSecondary() {
+        $this->assertEquals("frames",           $this->ps->cleanConverterNamePiece("....frames", $this->CHARACTERS_ALLOWED_IN_SECONDARY));
+    }
 
-        // extreme examples of messy input - the resulting converter names are invalid
-        $this->assertEquals("//frames",         $this->ps->cleanConverterNamePiece("..//.frames",     CHARACTERS_ALLOWED_IN_SECONDARY));
-        $this->assertEquals("/./default/./",    $this->ps->cleanConverterNamePiece("/./default/.##/", CHARACTERS_ALLOWED_IN_TERTIARY));
-        $this->assertEquals("//default//",      $this->ps->cleanConverterNamePiece("//default//",     CHARACTERS_ALLOWED_IN_TERTIARY));
 
+
+    /**
+     * extreme examples of messy input - the resulting converter names are invalid
+     */
+    public function testExtremeExampleAndInvalidSecondary() {
+        $this->assertEquals("//frames",         $this->ps->cleanConverterNamePiece("..//.frames",     $this->CHARACTERS_ALLOWED_IN_SECONDARY));
+    }
+    public function testExtremeExampleAndInvalidTertiaryA() {
+        $this->assertEquals("/./default/./",    $this->ps->cleanConverterNamePiece("/./default/.##/", $this->CHARACTERS_ALLOWED_IN_TERTIARY));
+    }
+    public function testExtremeExampleAndInvalidTertiaryB() {
+        $this->assertEquals("//default//",      $this->ps->cleanConverterNamePiece("//default//",     $this->CHARACTERS_ALLOWED_IN_TERTIARY));
     }
 
 }
 
 // Call phpDocumentor_setupTest::main() if this source file is executed directly.
-if (PHPUnit_MAIN_METHOD == "phpDocumentor_setupTest::main") {
+if (PHPUnit_MAIN_METHOD == "Documentation_tests_bug1620470::main") {
     phpDocumentor_setupTest::main();
 }
 ?>
