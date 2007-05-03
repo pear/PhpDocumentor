dbdf45e71310e7321e60daa42ade69ed77d6024d
Chuck Burgess <ashnazg@php.net>
2007-05-03 21:46:48 +0000

    new unit tests for IntermediateParser->addPrivatePage() method;
    git-svn-id: http://svn.php.net/repository/pear/packages/PhpDocumentor/trunk@234887 c90b9560-bf6c-de11-be94-00142212c4b1

--- AllTests.php
+++ AllTests.php
@@ -5,11 +5,22 @@ if (!defined('PHPUnit_MAIN_METHOD')) {
  
 require_once 'PHPUnit/Framework.php';
 require_once 'PHPUnit/TextUI/TestRunner.php';
- 
+
+/**
+ * You must add each additional class-level test suite file here
+ */
 require_once 'Documentation/tests/phpDocumentorSetupTests.php';
 require_once 'Documentation/tests/phpDocumentorTParserTests.php';
+require_once 'Documentation/tests/IntermediateParserTests.php';
 require_once 'Documentation/tests/ParserClassTests.php';
 require_once 'Documentation/tests/ParserPageTests.php';
+/**
+ * Base directory of code being PhpDoc'd
+ * 
+ * Needed by some of the objects being tested in the suites
+ * @since 1.4.0a2
+ */
+define("PHPDOCUMENTOR_BASE", '.');
 
 class AllTests
 {
@@ -21,8 +32,12 @@ class AllTests
     public static function suite()
     {
         $suite = new PHPUnit_Framework_TestSuite('PhpDocumentor Full Suite of Unit Tests');
+        /**
+         * You must add each additional class-level test suite name here
+         */
         $suite->addTest(Documentation_tests_phpDocumentorSetupTests::suite());
         $suite->addTest(Documentation_tests_phpDocumentorTParserTests::suite());
+        $suite->addTest(Documentation_tests_IntermediateParserTests::suite());
         $suite->addTest(Documentation_tests_ParserClassTests::suite());
         $suite->addTest(Documentation_tests_ParserPageTests::suite());
         return $suite;
