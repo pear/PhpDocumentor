90c77cf702d7db28c1950717a6e4592e37a8bcf5
Chuck Burgess <ashnazg@php.net>
2007-05-03 15:19:26 +0000

    AllTests.php just needs to be in top-level codebase dir, not also in top-level test_dir, in order for PEAR RUN-TESTS to find and use it;
    git-svn-id: http://svn.php.net/repository/pear/packages/PhpDocumentor/trunk@234850 c90b9560-bf6c-de11-be94-00142212c4b1

--- AllTests.php
+++ AllTests.php
@@ -6,15 +6,11 @@ if (!defined('PHPUnit_MAIN_METHOD')) {
 require_once 'PHPUnit/Framework.php';
 require_once 'PHPUnit/TextUI/TestRunner.php';
  
-/**
 require_once 'Documentation/tests/phpDocumentorSetupTests.php';
 require_once 'Documentation/tests/phpDocumentorTParserTests.php';
 require_once 'Documentation/tests/ParserClassTests.php';
 require_once 'Documentation/tests/ParserPageTests.php';
-*/
 
-require_once 'Documentation/tests/AllTests.php';
- 
 class AllTests
 {
     public static function main()
@@ -25,13 +21,10 @@ class AllTests
     public static function suite()
     {
         $suite = new PHPUnit_Framework_TestSuite('PhpDocumentor Full Suite of Unit Tests');
-        $suite->addTest(Documentation_tests_AllTests::suite());
-/**
         $suite->addTest(Documentation_tests_phpDocumentorSetupTests::suite());
         $suite->addTest(Documentation_tests_phpDocumentorTParserTests::suite());
         $suite->addTest(Documentation_tests_ParserClassTests::suite());
         $suite->addTest(Documentation_tests_ParserPageTests::suite());
-*/
         return $suite;
     }
 }
