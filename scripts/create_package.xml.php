81ba5f2ca5a306e23de005893966ebb65d25eb29
Chuck Burgess <ashnazg@php.net>
2007-07-20 00:51:34 +0000

    prep for final 1.4.0 release;
    git-svn-id: http://svn.php.net/repository/pear/packages/PhpDocumentor/trunk@240010 c90b9560-bf6c-de11-be94-00142212c4b1

--- scripts/create_package.xml.php
+++ scripts/create_package.xml.php
@@ -6,19 +6,50 @@ PEAR::setErrorHandling(PEAR_ERROR_DIE);
 
 $packagedir = dirname(dirname(__FILE__));
 $notes = '
-Includes no PEAR items:
+Includes these PEAR items:
+Req #10670: Don&apos;t Die on Empty Tag Descriptions
+Bug #10864: PhpDocumentor dies when parsing illegal page-level docblock
+Bug #10870: Dead links generated with HTML:Smarty:PHP
+Bug #10871: Wrong rendering of inline {@internal}}
+Bug #10910: DOM templates create incorrect trees
+Bug #11409: PHP Notices accompany Global Never Found errors
+Doc #10675: Clear some errors.html items
+Doc #10730: Replace Sourceforge Trackers with PEAR
+Doc #10914: Can&apos;t use nested tags inside &lt;kbd&gt; or &lt;samp&gt;
+Doc #10972: @method does not work
+Doc #11032: param type1|type2 not documented
+Doc #11143: Provide a Minimal Tutorial File Setup Example
 
 Includes these Sourceforge items:
-- [1733938] regression: "-" not allowed anymore in converter (file)name
-- [1733936] "phpdoc" does not return with error code "0" on success
+- [1733938] regression: &quot;-&quot; not allowed anymore in converter (file)name
+- [1733936] &quot;phpdoc&quot; does not return with error code &quot;0&quot; on success
 - [1733345] inline @link tag renders wrong way
+- [1724990] @static tag example
+- [1724884] object return parameter value
 - [1722096] DocBlock template example does not work
+- [1720764] documentation update needed
+- [1672589] missing elements, bad links with HTML:Smarty:HandS
+- [1647423] memory_limit: bad if-clause
+- [1634153] require/include with concatenated file name
+- [1632496] {@internal}} becomes wrong on documentation
+- [1622538] List undocumented classes/functions
+- [1602988] Added --ignore-symlinks option
+- [1588942] Allow memory_limit setting in phpdoc.ini
+- [1588936] Show INI Path at Startup
+- [1585840] Refactored RETURNs #3
+- [1574047] Refactored RETURNs #2
+- [1574043] Refactored RETURNs #1
+- [1543289] Apply ignore patterns to subdir of dir, instead of full path
+- [1543287] Use predefined PHP env var in phpdoc script
+- [1540666] patch for @property, @property-read and @property-write tags
+- [1124133] &lt;code&gt; and @include....
+- [1044752] List undocumented classes/functions
 
 ';
-$version = '1.4.0RC2';
-$release_stability = 'beta';
+$version = '1.4.0';
+$release_stability = 'stable';
 $api = '1.4.0';
-$api_stability = 'beta';
+$api_stability = 'stable';
 $options = array(
 'baseinstalldir' => 'PhpDocumentor',
 'version' => $version,
@@ -61,6 +92,7 @@ $options = array(
         'Release-1.4.0alpha2' => 'doc',
         'Release-1.4.0rc1' => 'doc',
         'Release-1.4.0rc2' => 'doc',
+        'Release-1.4.0' => 'doc',
         'pear-phpdoc' => 'script',
         'pear-phpdoc.bat' => 'script',
         'HTML_TreeMenu-1.1.2/TreeMenu.php' => 'php',
