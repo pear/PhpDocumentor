f61031dae93581c2c1c87ec860f11f119f3378b3
Chuck Burgess <ashnazg@php.net>
2007-08-28 20:25:46 +0000

    correct hardcoded reference to 1.4.0a2 with current 1.4.0;
    git-svn-id: http://svn.php.net/repository/pear/packages/PhpDocumentor/trunk@241889 c90b9560-bf6c-de11-be94-00142212c4b1

--- phpDocumentor/common.inc.php
+++ phpDocumentor/common.inc.php
@@ -43,7 +43,7 @@ if ('@PEAR-DIR@' != '@'.'PEAR-DIR@')
     /** @ignore */
     define("PHPDOCUMENTOR_VER","@VER@");
 } else {
-    define("PHPDOCUMENTOR_VER","1.4.0a2");
+    define("PHPDOCUMENTOR_VER","1.4.0");
 }
 /** phpDocumentor version */
 define("PHPDOCUMENTOR_WEBSITE","http://www.phpdoc.org");
