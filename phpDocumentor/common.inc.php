82f6e355fc67d8d9996acbc8b9397f51cee66219
Chuck Burgess <ashnazg@php.net>
2007-10-06 22:57:15 +0000

    PEAR CS cleanup
    git-svn-id: http://svn.php.net/repository/pear/packages/PhpDocumentor/trunk@243727 c90b9560-bf6c-de11-be94-00142212c4b1

--- phpDocumentor/common.inc.php
+++ phpDocumentor/common.inc.php
@@ -6,7 +6,7 @@
  * 
  * PHP versions 4 and 5
  *
- * Copyright (c) 2001-2006 Gregory Beaver
+ * Copyright (c) 2001-2007 Gregory Beaver
  * 
  * LICENSE:
  * 
@@ -25,81 +25,98 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  *
- * @package    phpDocumentor
- * @author     Greg Beaver <cellog@php.net>
- * @copyright  2001-2006 Gregory Beaver
- * @license    http://www.opensource.org/licenses/lgpl-license.php LGPL
- * @version    CVS: $Id$
+ * @category  ToolsAndUtilities
+ * @package   phpDocumentor
+ * @author    Greg Beaver <cellog@php.net>
+ * @copyright 2001-2007 Gregory Beaver
+ * @license   http://www.opensource.org/licenses/lgpl-license.php LGPL
+ * @version   CVS: $Id$
  * @filesource
- * @link       http://www.phpdoc.org
- * @link       http://pear.php.net/PhpDocumentor
- * @see        parserDocBlock, parserInclude, parserPage, parserClass
- * @see        parserDefine, parserFunction, parserMethod, parserVar
- * @since      1.0rc1
+ * @link      http://www.phpdoc.org
+ * @link      http://pear.php.net/PhpDocumentor
+ * @see       parserDocBlock, parserInclude, parserPage, parserClass
+ * @see       parserDefine, parserFunction, parserMethod, parserVar
+ * @since     1.0rc1
+ * @todo      CS cleanup - change package to PhpDocumentor
+ * @todo      CS cleanup - rename constant to TOKENIZER_EXT
  */
-/** phpDocumentor version */
-if ('@PEAR-DIR@' != '@'.'PEAR-DIR@')
-{
+
+/* phpDocumentor version */
+if ('@PEAR-DIR@' != '@'.'PEAR-DIR@') {
     /** @ignore */
-    define("PHPDOCUMENTOR_VER","@VER@");
+    define("PHPDOCUMENTOR_VER", "@VER@");
 } else {
-    define("PHPDOCUMENTOR_VER","1.4.0");
+    define("PHPDOCUMENTOR_VER", "1.4.0");
 }
-/** phpDocumentor version */
-define("PHPDOCUMENTOR_WEBSITE","http://www.phpdoc.org");
-define('SMART_PATH_DELIMITER', DIRECTORY_SEPARATOR ); // set the correct path delimiter
-define('tokenizer_ext', extension_loaded('tokenizer') && version_compare(phpversion(),"4.3.0",">="));
+
+/* phpDocumentor URL */
+define("PHPDOCUMENTOR_WEBSITE", "http://www.phpdoc.org");
+
+// set the correct path delimiter
+define('SMART_PATH_DELIMITER', DIRECTORY_SEPARATOR); 
+
+define('tokenizer_ext', extension_loaded('tokenizer') 
+    && version_compare(phpversion(), "4.3.0", ">="));
+
 // we just replace all the \ with / so that we can just operate on /
-define('PATH_DELIMITER', '/' ); // set the correct path delimiter
-define('PHPDOCUMENTOR_WINDOWS',substr(PHP_OS, 0, 3) == 'WIN');
-define('_IN_PHP5', phpversion() == '5.0.0RC1-dev' || phpversion() == '5.0.0RC2-dev' ||
-    version_compare(phpversion(), '5.0.0', 'ge'));
-if ('@VER@' != '@'.'VER@')
-{
-    if (_IN_PHP5) {
-        require_once 'PhpDocumentor/phpDocumentor/clone5.inc.php';
-    } else {
-        require_once 'PhpDocumentor/phpDocumentor/clone.inc.php';
-    }
-} else {
-    if (_IN_PHP5) {
-        require_once dirname(__FILE__) . '/clone5.inc.php';
-    } else {
-        require_once dirname(__FILE__) . '/clone.inc.php';
-    }
+define('PATH_DELIMITER', '/'); // set the correct path delimiter
+
+define('PHPDOCUMENTOR_WINDOWS', substr(PHP_OS, 0, 3) == 'WIN');
+
+define('_IN_PHP5', 
+    phpversion() == '5.0.0RC1-dev' || phpversion() == '5.0.0RC2-dev' 
+    || version_compare(phpversion(), '5.0.0', 'ge'));
+
+// determine which "clone" class to set, based on PHP major version
+$cloneClassDir  = 'PhpDocumentor' . DIRECTORY_SEPARATOR . 'phpDocumentor';
+$cloneClassFile = 'clone.inc.php';
+if ('@VER@' == '@'.'VER@') {
+    // we're _not_ in a PEAR installation
+    $cloneClassDir = dirname(__FILE__);
 }
+if (_IN_PHP5) {
+    // we _are_ in PHP5
+    $cloneClassFile = 'clone5.inc.php';
+}
+require_once $cloneClassDir . DIRECTORY_SEPARATOR . $cloneClassFile;
+
+// make arg arrays available
 if (isset($_SERVER['argv'])) {
     $argv = $_SERVER['argv'];
     $argc = $_SERVER['argc'];
 }
 
-/** used in phpdoc.php and new_phpdoc.php */
+/**
+ * used in phpdoc.php and new_phpdoc.php 
+ *
+ * @param string $directory a directory string
+ *
+ * @return array an array of directory contents
+ * @todo CS cleanup - rename function to PhpDocumentor_ConfigFileList
+ */
 function phpDocumentor_ConfigFileList($directory)
 {
     $ret = array();
-    if (@is_dir($directory))
-    {
+    if (@is_dir($directory)) {
         $ret = array();
-        $d = @dir($directory); // thanks to Jason E Sweat (jsweat@users.sourceforge.net) for fix
-        while($d && $entry=$d->read())
-        {
+
+        // thanks to Jason E Sweat (jsweat@users.sourceforge.net) for fix
+        $d = @dir($directory); 
+
+        while ($d && $entry=$d->read()) {
             $getentry = false;
-            if (strcmp($entry,".") != 0 && strcmp($entry,"..") != 0)
-            {
-                if (substr($entry,0,1) != ".") $getentry = true;
+            if (strcmp($entry, ".") != 0 && strcmp($entry, "..") != 0) {
+                if (substr($entry, 0, 1) != ".") $getentry = true;
             }
-            if ($getentry == true)
-            {
-                if (strpos($entry,'.ini'))
-                if (is_file($directory . PATH_DELIMITER . $entry))
-                {
-                    $ret[] = str_replace('.ini','',$entry);
+            if ($getentry == true) {
+                if (strpos($entry, '.ini'))
+                if (is_file($directory . PATH_DELIMITER . $entry)) {
+                    $ret[] = str_replace('.ini', '', $entry);
                 }
             }
         }
         if ($d) $d->close();
-    } else
-    {
+    } else {
     }
     return $ret;
 }
@@ -118,64 +135,59 @@ function phpDocumentor_ConfigFileList($directory)
  * </pre>
  *
  * and return an associative array(MYVAR => array(value1, value2, value3))
+ *
+ * @param string $filename         full path to the ini file
+ * @param bool   $process_sections add an associative index 
+ *                                 for each section [in brackets]
+ *
  * @return array
- * @param string full path to the ini file
- * @param boolean add an associative index for each section [in brackets]
+ * @todo CS cleanup - rename function to PhpDocumentor_parse_ini_file
  */
 function phpDocumentor_parse_ini_file($filename, $process_sections = false)
 {
     $ini_array = array();
-    $sec_name = "";
-    $lines = @file($filename);
-    if( !$lines) return $lines;
-    foreach($lines as $line)
-    {
+    $sec_name  = "";
+    $lines     = @file($filename);
+    if (!$lines) return $lines;
+    foreach ($lines as $line) {
         // code by Greg Beaver, ignore comments
         if ($line[0] == ';') continue;
         $line = trim($line);
         
-        if($line == "")
-        {
+        if ($line == "") {
             continue;
         }
-        if($line[0] == "[" && $line[strlen($line) - 1] == "]")
-        {
+        if ($line[0] == "[" && $line[strlen($line) - 1] == "]") {
             $sec_name = substr($line, 1, strlen($line) - 2);
-        } else
-        {
-            if (strpos($line,"="))
-            {
-                $pos = strpos($line, "=");
+        } else {
+            if (strpos($line, "=")) {
+                $pos      = strpos($line, "=");
                 $property = trim(substr($line, 0, $pos));
                 // code by Greg Beaver
                 if (substr($property, 0, 1) == '"' && substr($property, -1) == '"') {
-                    $property = stripcslashes(substr($property,1,count($property) - 2));
+                    $property = 
+                        stripcslashes(substr($property, 1, count($property) - 2));
                 }
                 $value = trim(substr($line, $pos + 1));
                 if ($value == 'false') $value = false;
                 if ($value == 'true') $value = true;
-                if (substr($value, 0, 1) == '"' && substr($value,-1) == '"')
-                {
-                    $value = stripcslashes(substr($value,1,count($value) - 2));
+                if (substr($value, 0, 1) == '"' && substr($value, -1) == '"') {
+                    $value = stripcslashes(substr($value, 1, count($value) - 2));
                 }
                 // done additions
                 
-                if($process_sections)
-                {
+                if ($process_sections) {
                     if ($sec_name != '')
                     $ini_array[$sec_name][$property] = $value;
                     else
                     $ini_array[$property] = $value;
-                } else
-                {
+                } else {
                     $ini_array[$property] = $value;
                 }
-            } else
-            {
+            } else {
                 // code by Greg Beaver
                 if (trim($line[0]) == ';') continue;
-                if($process_sections)
-                {
+                if ($process_sections) {
                     $ini_array[$sec_name][] = trim($line);
                 }
                 // done additions
@@ -186,39 +198,71 @@ function phpDocumentor_parse_ini_file($filename, $process_sections = false)
 }
 
 
-if (!function_exists('array_key_exists'))
-{
-/** @ignore */
-function array_key_exists($key, $search)
-{
-    foreach($search as $keys => $nul)
+/**
+ * construct an "array_key_exists()" method
+ * if the runtime PHP version doesn't have one
+ * 
+ * @todo CS Cleanup - can't avoid "prefixed by package" error
+ * @todo depend on PHP_Compat for this?
+ */
+if (!function_exists('array_key_exists')) {
+    /** 
+     * Determines if a given key exists in a given array
+     *
+     * @param mixed $key    key to search for
+     * @param array $search the array of keys to search
+     *
+     * @return bool whether or not the key was found
+     * @ignore
+     */
+    function array_key_exists($key, $search)
     {
-        if ($key == $keys) return true;
-    }
-    return false;
-}
-}
-if (!function_exists('is_a'))
-{
-/** @ignore */
-function is_a($classname, $classquery)
-{
-    $father = get_parent_class($classname);
-    if (strtolower($father) == strtolower($classquery))
-    {
-        return true;
-    } elseif (!empty($father))
-    {
-        return is_a($father, $classquery);
-    } else
-    {
+        foreach ($search as $keys => $nul) {
+            if ($key == $keys) return true;
+        }
         return false;
     }
 }
+
+/**
+ * construct an "is_a()" method
+ * if the runtime PHP version doesn't have one
+ * 
+ * @todo CS Cleanup - can't avoid "prefixed by package" error
+ * @todo depend on PHP_Compat for this?
+ */
+if (!function_exists('is_a')) {
+    /**
+     * Determines if one item "is" an object of the other item
+     *
+     * @param string $classname  the class in question
+     * @param string $classquery the "is it a" class
+     *
+     * @return bool whether or not the class "is" one
+     * @ignore
+     */
+    function is_a($classname, $classquery)
+    {
+        $father = get_parent_class($classname);
+        if (strtolower($father) == strtolower($classquery)) {
+            return true;
+        } elseif (!empty($father)) {
+            return is_a($father, $classquery);
+        } else {
+            return false;
+        }
+    }
 }
 
 
-/** Debugging output */
+/**
+ * Debugging output
+ *
+ * @param string $s the "debug message" string to echo out
+ *
+ * @return void
+ * @todo CS Cleanup - can't avoid "prefixed by package" error
+ */
 function debug($s)
 {
     echo "$s\n";
@@ -226,21 +270,21 @@ function debug($s)
 
 /**
  * Returns a formatted var_dump for debugging purposes.
+ *
  * @param string $s string to display
- * @param mixed $v variable to display with var_dump()
- * @param mixed $v... unlimited number of additional variables to display with var_dump()
+ * @param mixed  $v unlimited number of variables to display with var_dump()
+ *
+ * @return void
  */
 function fancy_debug($s,$v)
 {
     if (isset($GLOBALS['dont_debug']) && $GLOBALS['dont_debug']) return;
     debug($s."\n\n</pre><blockquote><pre>");
     var_dump($v);
-    if (func_num_args()>2)
-    {
-        for($i=2;$i<func_num_args();$i++)
-        {
+    if (func_num_args()>2) {
+        for ($i=2;$i<func_num_args();$i++) {
             $a = func_get_arg($i);
-//            debug(" ");
+            // debug(" ");
             var_dump($a);
         }
     }
@@ -251,6 +295,11 @@ function fancy_debug($s,$v)
  * Returns a lower-cased version of get_class for PHP 5
  *
  * get_class() returns case as declared in the file in PHP 5
+ *
+ * @param object $object the object to get the classname for
+ *
+ * @return string the class name of the given object
+ * @todo CS cleanup - rename function to PhpDocumentor_get_class
  */
 function phpDocumentor_get_class($object)
 {
