681afe81f6a99b534e089503c790f4e67bfbf26a
Chuck Burgess <ashnazg@php.net>
2007-10-11 03:30:34 +0000

    PEAR CS cleanup for Publisher.inc
    git-svn-id: http://svn.php.net/repository/pear/packages/PhpDocumentor/trunk@244033 c90b9560-bf6c-de11-be94-00142212c4b1

--- scripts/create_package.xml.php
+++ scripts/create_package.xml.php
@@ -144,6 +144,7 @@ $pfm2->addReplacement('phpDocumentor/common.inc.php', 'package-info', '@VER@', '
 $pfm2->addReplacement('phpDocumentor/common.inc.php', 'pear-config', '@PEAR-DIR@', 'php_dir');
 $pfm2->addReplacement('phpDocumentor/IntermediateParser.inc', 'package-info', '@VER@', 'version');
 $pfm2->addReplacement('phpDocumentor/IntermediateParser.inc', 'pear-config', '@PEAR-DIR@', 'php_dir');
+$pfm2->addReplacement('phpDocumentor/Publisher.inc', 'package-info', '@VER@', 'version');
 $pfm2->addReplacement('user/pear-makedocs.ini', 'pear-config', '@PEAR-DIR@', 'php_dir');
 $pfm2->addReplacement('user/pear-makedocs.ini', 'pear-config', '@DOC-DIR@', 'doc_dir');
 $pfm2->addReplacement('user/pear-makedocs.ini', 'package-info', '@VER@', 'version');
