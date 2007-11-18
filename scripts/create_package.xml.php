b96fbe4c83372ba1fdcf3540f7f8e62bebffa7a3
Chuck Burgess <ashnazg@php.net>
2007-11-18 17:44:15 +0000

    PEAR CS cleanup for HighlightParser.inc
    git-svn-id: http://svn.php.net/repository/pear/packages/PhpDocumentor/trunk@246387 c90b9560-bf6c-de11-be94-00142212c4b1

--- scripts/create_package.xml.php
+++ scripts/create_package.xml.php
@@ -143,6 +143,7 @@ $pfm2->addReplacement('phpDocumentor/Errors.inc', 'package-info', '@VER@', 'vers
 $pfm2->addReplacement('phpDocumentor/EventStack.inc', 'package-info', '@VER@', 'version');
 $pfm2->addReplacement('phpDocumentor/common.inc.php', 'package-info', '@VER@', 'version');
 $pfm2->addReplacement('phpDocumentor/common.inc.php', 'pear-config', '@PEAR-DIR@', 'php_dir');
+$pfm2->addReplacement('phpDocumentor/HighlightParser.inc', 'package-info', '@VER@', 'version');
 $pfm2->addReplacement('phpDocumentor/IntermediateParser.inc', 'package-info', '@VER@', 'version');
 $pfm2->addReplacement('phpDocumentor/IntermediateParser.inc', 'pear-config', '@PEAR-DIR@', 'php_dir');
 $pfm2->addReplacement('phpDocumentor/phpDocumentorTParser.inc', 'package-info', '@VER@', 'version');
