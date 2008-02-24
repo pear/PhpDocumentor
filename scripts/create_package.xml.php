a6c7ec33e40c8753bd7b997cdbb2ba8b096d6150
Chuck Burgess <ashnazg@php.net>
2008-02-24 02:25:32 +0000

    PEAR CS cleanup for LinkClasses.inc
    git-svn-id: http://svn.php.net/repository/pear/packages/PhpDocumentor/trunk@253640 c90b9560-bf6c-de11-be94-00142212c4b1

--- scripts/create_package.xml.php
+++ scripts/create_package.xml.php
@@ -111,6 +111,7 @@ $pfm2->addReplacement('phpDocumentor/common.inc.php', 'pear-config', '@PEAR-DIR@
 $pfm2->addReplacement('phpDocumentor/HighlightParser.inc', 'package-info', '@VER@', 'version');
 $pfm2->addReplacement('phpDocumentor/IntermediateParser.inc', 'package-info', '@VER@', 'version');
 $pfm2->addReplacement('phpDocumentor/IntermediateParser.inc', 'pear-config', '@PEAR-DIR@', 'php_dir');
+$pfm2->addReplacement('phpDocumentor/LinkClasses.inc', 'package-info', '@VER@', 'version');
 $pfm2->addReplacement('phpDocumentor/phpDocumentorTParser.inc', 'package-info', '@VER@', 'version');
 $pfm2->addReplacement('phpDocumentor/phpDocumentorTWordParser.inc', 'package-info', '@VER@', 'version');
 $pfm2->addReplacement('phpDocumentor/ProceduralPages.inc', 'package-info', '@VER@', 'version');
